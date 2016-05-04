b41e72ddd011f0ed8c507ce285d3bfad73d4dfe8
Amazos <olegi_g@yahoo.com>
2016-05-04 19:22:02 +0400

    temporarry delete curl examples
    temporarry delete curl examples

--- curl examples/Route/view_single_route.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET url=http://www.route4me.com
-SET apikey=11111111111111111111111111111111
-SET routeid=5C15E83A4BE005BCD1537955D28D51D7
-
-ECHO ON
-curl -o file1.txt -g -X GET "%URL%/api.v4/route.php?route_id=%routeid%&api_key=%apikey%" 
-
-timeout /t 30
