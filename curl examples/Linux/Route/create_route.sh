e4a28ad372da1fd713f6a1be4fcdc935acddee11
Amazos <olegi_g@yahoo.com>
2016-05-13 17:24:27 +0400

    updated create_route Linux examples
    updated create_route Linux examples

--- curl examples/Linux/Route/create_route.sh
+++ curl examples/Linux/Route/create_route.sh
@@ -2,10 +2,9 @@
 
 url=http://www.route4me.com/api.v4/optimization_problem.php
 apikey=11111111111111111111111111111111
+opi=31AC533ECF9429CDECCECC530745F7BD
 
-# permanently returns "errors":["Point is not allowed for test account"] - needs clarification
-
-curl -o file1.txt -g -X POST -H "Content-Type: application/json" -d "@new_route_data.json" "$url?api_key=$apikey"
+curl -o file1.txt -g -X POST -H "Content-Type: application/json" -d "@new_route_data.json" "$url?api_key=$apikey&optimization_problem_id=$opi"
 
 echo "Finished..."
 
