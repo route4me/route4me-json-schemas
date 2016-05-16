832a23474d7a7d6d606d2935dfea7b7de8150fbc
Amazos <olegi_g@yahoo.com>
2016-05-16 18:33:08 +0400

    moved files to other repositories
    moved files to other repositories

--- curl examples/Windows/Route/merge_route.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-SET url=https://www.route4me.com
-SET apikey=11111111111111111111111111111111
-
-ECHO ON
-curl -o file1.txt -g -k -X POST -d '@merge_route_data.json' "%URL%/actions/merge_routes.php?api_key=%apikey%" 
-
-timeout /t 30
--- curl examples/Windows/Route/view_routes.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-SET url=http://www.route4me.com
-SET apikey=11111111111111111111111111111111
-
-ECHO ON
-curl -o file1.txt -g -X GET "%URL%/api.v4/route.php?api_key=%apikey%&limit=20&offset=100" 
-
-timeout /t 30
